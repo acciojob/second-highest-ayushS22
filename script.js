a51b0d0d82ac911fa7d301f8c62d891185e0ab49
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-06-01 13:57:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-//your JS code here. If required.
-function secondHighest(arr) {
-    if (arr.length < 2) return -Infinity;
+ if (arr.length < 2) return -Infinity;
 
-    let max = -Infinity;
-    let secondMax = -Infinity;
+  let max = -Infinity;
+  let secondMax = -Infinity;
 
-    for (let num of arr) {
-        if (num > max) {
-            secondMax = max;
-            max = num;
-        } else if (num > secondMax && num < max) {
-            secondMax = num;
-        }
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num < max && num > secondMax) {
+      secondMax = num;
     }
+  }
 
-    return secondMax === -Infinity ? -Infinity : secondMax;
-}
+  return secondMax === -Infinity ? -Infinity : secondMax;
